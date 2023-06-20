e9131b7fae4ab7d4f837d916c0b173d60b27618b
Carson Black <carsontimothyblack@gmail.com>
2023-06-20 15:30:51 -0400

    Adding book list project.

---
+++ book_list/src/App.js
@@ -0,0 +1,27 @@
+import "./index.css";
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    const updatedBooks = [
+      ...books,
+      {
+        id: Math.round(Math.random() * 9999),
+        title: title
+      }
+    ];
+    setBooks(updatedBooks)
+  };
+
+  return (
+    <div>
+      {books.length}
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
+}
+
+export default App;
